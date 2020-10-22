8277b64f2dde426160f191e17bd8ba0e53de8757
Felipe <felipe@webgoal.com.br>
2020-10-21 22:39:33 -0300

    .

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Button from '../components/button';
+import Swiper from 'react-native-swiper'
+
+let hist = [];
 
 export default function Main() {
   const [result, setResult] = useState('0');
   const [memo, setMemo] = useState('0');
 
-  console.log(memo)
   useEffect(() => {
     if (result.slice(0, 5) === 'Error' && result.length > 5)
       setResult(result.slice(5, result.length))
@@ -16,79 +18,104 @@ export default function Main() {
       setResult(result.slice(1, result.length))
   }, [result])
 
+
   return (
-    <View style={styles.container}>
-      <View style={styles.display}>
-        <Text style={styles.result}>{result}</Text>
+    <Swiper loop={false} showsButtons={false} showsPagination={false}>
+      <View style={styles.history}>
+        <Text style={styles.textHistory}>Resultados:</Text>
+        <ScrollView>
+          {
+            hist.map((item) => (
+            <Text style={styles.textHistory}>{item}</Text>
+            ))
+          }
+        </ScrollView>
       </View>
-      <View style={styles.keyboard}>
-        <View style={styles.row}>
-          <Button number="c" highlight={true} pressButton={() => setResult('0')}/>
-          <Button number="del" highlight={true} pressButton={
-            () => { 
-              if (result.length === 1)
-                return setResult('0')
-              setResult(result.slice(0, result.length - 1))
-            }
-          }/>
-          <Button number="Mc" highlight={true} pressButton={() => setMemo('0')} />
-
+      <View style={styles.container}>
+        <View style={styles.display}>
+          <Text style={styles.result}>{result}</Text>
         </View>
-
-        <View style={styles.row}>
-          <Button number="x²" highlight={true} pressButton={() => setResult(eval(Math.pow(eval(result).toString(), 2)).toString())}/>
-          <Button number="log" highlight={true} pressButton={() => setResult(eval(Math.log(eval(result).toString())).toString())}/>
-          <Button number="√" highlight={true} pressButton={() => setResult(eval(Math.pow(eval(result).toString(), 0.5)).toString())}/>
-          <Button number="/" highlight={true} pressButton={() => setResult(result + '/')} />
-        </View>
-
-        <View style={styles.row}>
-          <Button number="7" pressButton={() => setResult(result + '7')} />
-          <Button number="8" pressButton={() => setResult(result + '8')}/>
-          <Button number="9" pressButton={() => setResult(result + '9')}/>
-          <Button number="x" highlight={true} pressButton={() => setResult(result + '*')}/>
-        </View>
-
-        <View style={styles.row}>
-          <Button number="4" pressButton={() => setResult(result + '4')}/>
-          <Button number="5" pressButton={() => setResult(result + '5')}/>
-          <Button number="6" pressButton={() => setResult(result + '6')}/>
-          <Button number="-" highlight={true} pressButton={() => setResult(result + '-')}/>
-        </View>
-
-        <View style={styles.row}>
-          <Button number="1" pressButton={() => setResult(result + '1')}/>
-          <Button number="2" pressButton={() => setResult(result + '2')}/>
-          <Button number="3" pressButton={() => setResult(result + '3')}/>
-          <Button number="+" highlight={true} pressButton={() => setResult(result + '+')}/>
-        </View>
-
-        <View style={styles.row}>
-          <Button number="M+" highlight={true} pressButton={
-            () => {
-              if(memo === '0')
-                return setMemo(result)
-
-              setResult(result + memo)
-            }
-          }/>
-          <Button number="0" pressButton={() => setResult(result + '0')}/>
-          <Button number="," pressButton={() => setResult(result + '.')}/>
-          <Button number="=" highlight={true}  pressButton={
-            () => {
-              try {
-                if(result === 'Error')
+        <View style={styles.keyboard}>
+          <View style={styles.row}>
+            <Button number="c" highlight={true} pressButton={() => setResult('0')}/>
+            <Button number="del" highlight={true} pressButton={
+              () => { 
+                if (result.length === 1)
                   return setResult('0')
-                setResult(eval(result).toString())
+                setResult(result.slice(0, result.length - 1))
               }
-              catch (e) {
-                setResult('Error')
-              }
-            }
             }/>
+            <Button number="Mc" highlight={true} pressButton={() => setMemo('0')} />
+
+          </View>
+
+          <View style={styles.row}>
+            <Button number="x²" highlight={true} pressButton={() => setResult(eval(Math.pow(eval(result).toString(), 2)).toString())}/>
+            <Button number="log" highlight={true} pressButton={() => setResult(eval(Math.log(eval(result).toString())).toString())}/>
+            <Button number="√" highlight={true} pressButton={() => setResult(eval(Math.pow(eval(result).toString(), 0.5)).toString())}/>
+            <Button number="/" highlight={true} pressButton={() => setResult(result + '/')} />
+          </View>
+
+          <View style={styles.row}>
+            <Button number="7" pressButton={() => setResult(result + '7')} />
+            <Button number="8" pressButton={() => setResult(result + '8')}/>
+            <Button number="9" pressButton={() => setResult(result + '9')}/>
+            <Button number="x" highlight={true} pressButton={() => setResult(result + '*')}/>
+          </View>
+
+          <View style={styles.row}>
+            <Button number="4" pressButton={() => setResult(result + '4')}/>
+            <Button number="5" pressButton={() => setResult(result + '5')}/>
+            <Button number="6" pressButton={() => setResult(result + '6')}/>
+            <Button number="-" highlight={true} pressButton={() => setResult(result + '-')}/>
+          </View>
+
+          <View style={styles.row}>
+            <Button number="1" pressButton={() => setResult(result + '1')}/>
+            <Button number="2" pressButton={() => setResult(result + '2')}/>
+            <Button number="3" pressButton={() => setResult(result + '3')}/>
+            <Button number="+" highlight={true} pressButton={() => setResult(result + '+')}/>
+          </View>
+
+          <View style={styles.row}>
+            <Button number="M+" highlight={true} pressButton={
+              () => {
+                if(memo === '0')
+                  return setMemo(result)
+
+                setResult(result + memo)
+              }
+            }/>
+            <Button number="0" pressButton={() => setResult(result + '0')}/>
+            <Button number="," pressButton={() => setResult(result + '.')}/>
+            <Button number="=" highlight={true}  pressButton={
+              () => {
+                try {
+                  if(result === 'Error')
+                    return setResult('0')
+                  setResult(eval(result).toString())
+                  hist.push(eval(result).toString())
+                }
+                catch (e) {
+                  setResult('Error')
+                }
+              }
+              }/>
+          </View>
         </View>
       </View>
-    </View>
+      <View style={styles.history}>
+        <Text style={styles.textHistory}>Créditos</Text>
+        <Text style={styles.textHistory}></Text>
+        <Text style={styles.textHistory}>Alunos:</Text>
+        <Text style={styles.textHistory}>Flávio Luis</Text>
+        <Text style={styles.textHistory}>Vinicius Bagode</Text>
+        <Text style={styles.textHistory}></Text>
+        <Text style={styles.textHistory}>Professor:</Text>
+        <Text style={styles.textHistory}>Will</Text>
+
+      </View>
+    </Swiper>
   );
 }
 
@@ -119,6 +146,16 @@ const styles = StyleSheet.create({
     height: '70%',
     backgroundColor: '#000000'
   },
+  history: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#111111',
+    fontSize: 30,
+    color: '#ff9f21', 
+    padding: 15,
+    paddingTop: 35
+  },
   row: {
     flex: 1,
     flexDirection: 'row'
@@ -127,5 +164,9 @@ const styles = StyleSheet.create({
     color: '#ff9f21',
     fontSize: 64,
     textAlign: 'right'
+  },
+  textHistory: {
+    fontSize: 30,
+    color: '#ff9f21'
   }
 });
